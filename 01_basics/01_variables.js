9a554db0c2f28e16876825b71fb64e678a77c63f
Shantanu1512 <78294632+Shantanu1512@users.noreply.github.com>
2024-05-01 10:34:56 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234;
+var accountEmail = "shantanu@gmail.com";
+let accountHolder = "Shantanu";
+city = "Kolhapur";
+
+// accountId = 2345;  Not allowed as it is const
+accountEmail = "shantu@gmail.com";
+accountHolder = "Shantu";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountHolder]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //This use to treat all the JS code as newer version.
+
+// alert(12); This works in browser but we are using nodejs so it won't work as JS engine resides in browser.
+
+/*
+number => 2 power 53 range
+after this bigInt comes in picture
+string = ""
+booleasn = true/false
+null = it is a value and it is an Object.
+undefined = not a value
+*/ 
