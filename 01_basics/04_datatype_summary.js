f3886d5a842e95374ddec61b788ea5b71587dbf7
Shantanu1512 <78294632+Shantanu1512@users.noreply.github.com>
2024-05-07 18:03:50 +0000

    datatypes summary

---
+++ 01_basics/04_datatype_summary.js
@@ -0,0 +1,31 @@
+/*
+
+Data types
+    Premitive
+        String
+        Boolean
+        Number
+        Null
+        Undefined
+
+        Symbol
+            const id = Symbol('123')
+            const cId = Symbol('123')
+            console.log(id === cId) even if values are same it will not return true
+
+        BigInt
+            const bigNum = 23241n   if we add n then it will be bigInt
+    NonPremitive
+        Arrays
+            const arry = ["Shantanu", "Aishwarya", "Pratik", "Bhumi", "Sandesh"]
+        Objects
+            const myObj = {
+                name: "shantanu"
+                age: 24
+            }
+        Functions
+            const myFun = function(){
+                
+            }
+
+*/
