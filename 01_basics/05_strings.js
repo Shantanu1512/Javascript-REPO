2e915307d86c1c75efcc7fe2a0667272f71d5912
Shantanu1512 <78294632+Shantanu1512@users.noreply.github.com>
2024-05-08 18:21:39 +0000

    strings operation

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+// const str1 = "hello";
+// const str2 = 49;
+
+// console.log(str1+str2+"third");
+
+const str = "  Hey this is string demo  ";
+const arr = ["one", "two", "three", "four", "five", "six", "seven"];
+
+console.log(str);
+console.log(str.trim());
+console.log(arr.slice(1,-2));
+console.log(str.split(' '));
