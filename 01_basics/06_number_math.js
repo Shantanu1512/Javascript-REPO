f1cad89df3653d666d129c334e1ba4db602f5a7d
Shantanu1512 <78294632+Shantanu1512@users.noreply.github.com>
2024-05-08 18:45:20 +0000

    math and nums

---
+++ 01_basics/06_number_math.js
@@ -0,0 +1,27 @@
+const number = 900
+
+const newnum = new Number(900)
+
+// console.log(number);
+// console.log(newnum);
+
+const n = 123.808
+
+// console.log(n.toPrecision(4));
+
+const num = 1000000
+
+// console.log(num.toLocaleString('en-IN'));
+
+//======================================  MATH  ============================================
+
+// console.log(Math.abs(-1));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.round(3.9));
+// console.log(Math.random());
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
