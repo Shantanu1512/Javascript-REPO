adb278b5e415510e8c287b51790903c55d0c1e2c
Shantanu1512 <78294632+Shantanu1512@users.noreply.github.com>
2024-05-09 13:47:42 +0000

    array part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+// const myArr = [1,2,3,4,5,6]
+
+// const myStr = ["hello", "This", "is", "String"]
+
+// const myTArr = new Array(1,2,2,2,3,4)
+
+// // myArr.pop()
+// // myArr.push(8)
+
+// // myArr.unshift(9)
+// // myArr.shift()
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+const myArr = [1,2,3,4,5,6,7]
+
+console.log("A", myArr);
+
+const arr1 = myArr.slice(1,3)
+console.log(arr1);
+console.log("B", myArr);
+
+const arr2 = myArr.splice(1,3)
+console.log(arr2);
+console.log("C", myArr);
